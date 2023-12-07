91ce60df6b29dba47a28913157a0e57b7ed2bae8
Jon2024 <jonathanhomik@gmail.com>
2023-12-06 23:36:38 -0500

    Updated lab 14
    Figured out how to get users and list to work

--- labs/lab_14/todo_list.js
+++ labs/lab_14/todo_list.js
@@ -1,28 +1,58 @@
 "use strict";
 
 const domain = "https://jsonplaceholder.typicode.com";
-const url = "https://jsonplaceholder.typicode.com/users";
 
 const displayUsers = async () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(e => console.log(e.message));
-}
+    try {
+      const response = await fetch(`${domain}/users`);
+      const users = await response.json();
+  
+      const usersSelect = document.getElementById("users");
 
-$("#document").ready( async () => {
-    const json = await displayUsers(url); //calls displayUsers
-
-    //let userOptions = '<option></option>'
-    //for(let users of json.name){
-       //userOptions += `<option>${users.name}</option>`;
-    //}
-    //$("#users").append(userOptions);
-
-    for( var i = 0; i < url.length; i++){
-        var select = document.getElementById('users');
-        var option = document.createElement('option');
-        option.text = url[i].name;
-        select.add(option);
+      users.forEach(user => {
+        const option = document.createElement("option");
+        option.value = user.id;
+        option.text = user.name;
+        usersSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Error fetching users:", error);
     }
-})
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    displayUsers();
+
+    document.getElementById("view_list").addEventListener("click", async () => {
+      const userId = document.getElementById("users").value;
+  
+      try {
+        const response = await fetch(`${domain}/todos?userId=${userId}`);
+        const todoList = await response.json();
+  
+        const listContainer = document.getElementById("list");
+        listContainer.innerHTML = "";
+  
+        if (todoList.length > 0) {
+          const table = document.createElement("table");
+          const header = table.insertRow();
+          header.insertCell().textContent = "ID";
+          header.insertCell().textContent = "Title";
+          header.insertCell().textContent = "Completed";
+  
+          todoList.forEach(todo => {
+            const row = table.insertRow();
+            row.insertCell().textContent = todo.id;
+            row.insertCell().textContent = todo.title;
+            row.insertCell().textContent = todo.completed ? "Yes" : "No";
+          });
+  
+          listContainer.appendChild(table);
+        } else {
+          listContainer.textContent = "No todos found for the selected user.";
+        }
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    });
+  });
